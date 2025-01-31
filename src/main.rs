8a04101dd542bfcc9250f4210cd3c7116a8d9b73
ZEITOUN Guilhem <guilhem.zeitoun@igh.cnrs.fr>
2025-01-31 16:45:42 +0100

    add legends

--- src/main.rs
+++ src/main.rs
@@ -1509,6 +1509,12 @@ fn mismatchgraph<T>(
             .legend(|(x, y)| {
                 PathElement::new(vec![(x, y), (x + 15, y)], full_palette::DEEPORANGE_200)
             });
+        chart.configure_series_labels()
+        .position(plotters::chart::SeriesLabelPosition::UpperRight)
+        .background_style(WHITE)
+        .border_style(BLACK.mix(0.8))
+        .draw()
+        .unwrap();
     }
     // To avoid the IO failure being ignored silently, we manually call the present function
     root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
