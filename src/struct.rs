8721c8803575689a580e60d3f93b7d8ad77624b3
Guilhem Zeitoun <guilhem.zeitoun@igh.cnrs.fr>
2025-06-04 08:57:55 +0200

    change reverse text

--- src/struct.rs
+++ src/struct.rs
@@ -435,7 +435,7 @@ impl Display for Strand {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Plus => write!(f, "FWD"),
-            Self::Minus => write!(f, "RVD"),
+            Self::Minus => write!(f, "REV"),
         }
     }
 }
